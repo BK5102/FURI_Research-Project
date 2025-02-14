e22a402546be60abab13aa6b3ed27a7845e7d92a
Bhavana Kannan <bhavanakannan06@gmail.com>
2025-02-13 18:08:04 -0800

    checked structure, cleaned up some data and removed duplicates

--- merged_data.py
+++ merged_data.py
@@ -14,7 +14,7 @@ physical_act = pd.read_csv("data/2005-2006/Physical_Activity.csv")
 plasma_fasting_insulin = pd.read_csv("data/2005-2006/Plasma_Fasting_Glucose_Insulin.csv")
 sleep_data = pd.read_csv("data/2005-2006/Sleep_Disorders.csv")
 
-#merged_data = pd.merge(blood_pressure_and_cholesterol, diabetes, on="SEQN", how="inner")
+# merged_data = pd.merge(blood_pressure_and_cholesterol, diabetes, on="SEQN", how="inner")
 
 masterlist = blood_pressure_and_cholesterol.merge(diabetes,left_on = "SEQN",right_on = "SEQN")
 masterlist = masterlist.merge(cholesterolldl_trigly_ApoB,left_on = "SEQN",right_on = "SEQN")
@@ -25,7 +25,39 @@ masterlist = masterlist.merge(physical_act,left_on = "SEQN",right_on = "SEQN")
 masterlist = masterlist.merge(plasma_fasting_insulin,left_on = "SEQN",right_on = "SEQN")
 masterlist = masterlist.merge(sleep_data,left_on = "SEQN",right_on = "SEQN")
 
-print(masterlist)
+
+
+## checking structure 
+    # print(masterlist.describe())
+    # print(masterlist.isnull().sum())
+
+
+
+## understanding features (columns) in the data
+# Separate numerical and categorical columns
+numerical_cols = masterlist.select_dtypes(include=["int64", "float64"]).columns
+categorical_cols = masterlist.select_dtypes(include=["object"]).columns
+
+    # print("Numerical Columns:", numerical_cols)
+    # print("Categorical Columns:", categorical_cols)
+
+
+
+## identifying data that needs cleaning and preprocessing 
+    # check for missing values and fill or drop missing values
+
+masterlist.fillna(masterlist.mean(), inplace=True)  # Fill with mean (for numerical)
+masterlist.fillna("Unknown", inplace=True)  # Fill with string (for categorical)
+masterlist.dropna(inplace=True)  # Remove rows with missing values
+
+    # check for duplicate rows, count and remove duplicates
+    #print(masterlist.duplicated().sum())
+masterlist.drop_duplicates(inplace=True)
+
+
+
+
+# print(masterlist)
 
 
 
