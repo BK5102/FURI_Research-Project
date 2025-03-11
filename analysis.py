766e5b3924766b889d1b8132974bc0cd98705aeb
Bhavana Kannan <bhavanakannan06@gmail.com>
2025-03-11 15:15:49 -0700

    analysis and observations part 1

--- analysis.py
+++ analysis.py
@@ -13,39 +13,49 @@ def main():
                       data_path="selected_features.csv", 
                       stats_path="selected_features_stats.csv")
     
-    print("test line")
-    
-    """ # 3. Perform additional analysis or visualization
-    numeric_cols = combined_selected_features.select_dtypes(include=[float, int]).columns
+    # 3. Perform additional analysis or visualization
+    visualization_of_data(combined_selected_features)
+
+def visualization_of_data(combined_selected_features):
+    independent_vars = ['LBDINSI']
+                        #'BPQ020'
+                        #'DIQ010', 'DIQ190A', 'DIQ190B', 'PAQ180', 'PADACTIV', 'PADDURAT', 
+                        #'SLD010H', 'LBXAPB', 'LBDINSI', 'LBXGH']
+    dependent_var_gender = 'RIAGENDR'
+    dependent_var_age = 'RIDAGEMN'
+
+    numeric_cols = combined_selected_features[independent_vars].select_dtypes(include=[float, int]).columns
+
+    #Distribution of independent variables by gender
     for col in numeric_cols:
+        #bins = range(1,11)
         plt.figure(figsize=(8, 4))
-        sns.histplot(combined_selected_features[col], kde=True)
-        plt.title(f"Distribution of {col}")
-        plt.xlabel(col)
+        sns.histplot(data=combined_selected_features, x=col, hue=dependent_var_gender, kde=True, palette="coolwarm", alpha=0.6, bins=8)
+        #plt.ylim(0, 1000)
+
+        plt.xticks(fontsize=8)
+        plt.title(f"Distribution of {col} by {dependent_var_gender}")
         plt.ylabel("Frequency")
+        plt.legend(title="Gender", labels=["Male", "Female"])
+        
+        #plt.xticks(bins)
+
+        plt.tight_layout()
+        plt.show()
+
+
+    #Distribution of independent variables by age
+    """ for col in numeric_cols:
+        #bins = range(1,11)
+        plt.figure(figsize=(8, 4))
+        sns.histplot(data=combined_selected_features, x=col, y=dependent_var_age, scatter_kws={'alpha':0.5}, line_kws={'color':'red'})
+        plt.title(f"Regression Plot of {col} vs Age (in months)")
         plt.show() """
+
 
 if __name__ == "__main__":
     main()
 
-
-""" # Determine which columns are numeric or categorical
-numeric_cols = combined_selected_features.select_dtypes(include=[np.number]).columns
-
-# Plot numeric distributions
-for col in numeric_cols:
-    plt.figure(figsize=(8, 4))
-    sns.histplot(combined_selected_features[col], kde=True)
-    plt.title(f"Distribution of {col}")
-    plt.xlabel(col)
-    plt.ylabel("Frequency")
-    plt.show() """
-
-""" sns.countplot(x="BPQ020", data=BPC_df)
-plt.title("Distribution of BPQ020 (High Blood Pressure)")
-plt.xlabel("High Blood Pressure (2=No, 1=Yes)")
-plt.ylabel("Count")
-plt.show() """
 
 
 
